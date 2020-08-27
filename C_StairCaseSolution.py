b7b0e4ab4302b8dbf0c55847f5cb6f51e6803f55
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-27 14:45:02 +0000

    Stair Case

---
+++ C_StairCaseSolution.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+#
+# Complete the staircase function below.
+#
+def staircase(n):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
