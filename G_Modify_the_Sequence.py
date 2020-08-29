a1b7fef6705d2971ccdec771675395369a4427f2
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-29 11:26:59 +0000

    Modify the sequence Still not complete

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -6,15 +6,40 @@ import sys
 #
 def modifySequence(arr):
   count = 0
-  N=len(arr)
-  for i in range(N-1):
-    if arr[i+1] <= arr[i]:
-      count = (count+1)
-  print(count)
+  #print(len(arr))
+  #arr = sorted(arr , reverse=False)
+  for i in range(len(arr)-1):
+    #if arr[i-1] > arr[i] and arr[i] > 0 :
+      #arr[i] = arr[i-1] + 1
+      #count = count + 1
+    if  arr[i + 1] < arr[i] and arr[i] > 0:
+      arr[i + 1] = arr[i] + 1
+      count = count + 1
+    #elif arr[i+1] <= arr[i] :
+      #arr[i+1] = arr[i+1] + 1
+      #count = count + 1
+    #elif arr[i+1] >= arr[i] and arr[i-1] == arr[i]:
+      #arr[i+1] = arr[i+1] + 1
+      #count = (count+2)
+    #if arr[i+1] <= arr[i] and arr[i-1] < arr[i]:
+      #arr[i+1] = arr[i+1] + 1
+      #count = (count+1)
+    #elif arr[i+1] <= arr[i] and arr[i-1] > arr[i]:
+      #arr[i+1] = arr[i+1] + 1
+      #count = (count+2)
+    #elif arr[i+1] >= arr[i] and arr[i-1] == arr[i]:
+      #arr[i+1] = arr[i+1] + 1
+      #count = (count+2)
+  return count
 
 
 
 if True:
-    result = modifySequence([1,2,2,3,4])
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = modifySequence(arr)
+    #result = modifySequence([1,2,2,3,4])
     print(result)
 
