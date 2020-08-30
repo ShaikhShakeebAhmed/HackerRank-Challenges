e8725669016f06789e9db3f300d7d44cc505c49c
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-30 19:17:12 +0000

    Multiple trials

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -5,31 +5,30 @@ import sys
 # Complete the modifySequence function below.
 #
 def modifySequence(arr):
-  count = 0
-  #print(len(arr))
-  #arr = sorted(arr , reverse=False)
+  count = 0  
+  testcount = 0
+  totallen = len(arr)
+  startSequence = 1000000000 - totallen
+  #arr = sorted(arr , reverse = True)
+  #print(arr)
   for i in range(len(arr)-1):
-    #if arr[i-1] > arr[i] and arr[i] > 0 :
-      #arr[i] = arr[i-1] + 1
-      #count = count + 1
-    if  arr[i + 1] < arr[i] and arr[i] > 0:
-      arr[i + 1] = arr[i] + 1
-      count = count + 1
-    #elif arr[i+1] <= arr[i] :
-      #arr[i+1] = arr[i+1] + 1
-      #count = count + 1
-    #elif arr[i+1] >= arr[i] and arr[i-1] == arr[i]:
-      #arr[i+1] = arr[i+1] + 1
-      #count = (count+2)
-    #if arr[i+1] <= arr[i] and arr[i-1] < arr[i]:
-      #arr[i+1] = arr[i+1] + 1
-      #count = (count+1)
-    #elif arr[i+1] <= arr[i] and arr[i-1] > arr[i]:
-      #arr[i+1] = arr[i+1] + 1
-      #count = (count+2)
-    #elif arr[i+1] >= arr[i] and arr[i-1] == arr[i]:
-      #arr[i+1] = arr[i+1] + 1
-      #count = (count+2)
+    if arr[i] >= 1000000000:
+      arr[i] = arr[i - 1]
+      
+    
+  for i in range(len(arr)-1):
+    if arr[i +  1] <= arr[i] :
+      arr[i+1] = arr[i] + 1
+      count += 1
+    
+    
+    
+    
+   
+      
+        
+      
+  print(arr)
   return count
 
 
