5ec2dc6119f65b2b287d61e8147d882e627a54ab
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-31 13:21:45 +0000

    Still Trying

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -12,63 +12,17 @@ def modifySequence(arr):
   #arr = sorted(arr , reverse = True)
   #print(arr)
   for i in range(len(arr)-1):
-    print(arr[i])
-    print("- 0")
-    if arr[i] < 1000000000:
-      if arr[i + 1] <= arr[i] and arr[i] > arr[i - 1]:
-        if arr[i] > arr[i - 1]:
-          print(arr[i - 1])
-          print(arr[i])
-          print(arr[i + 1])
-          #arr[i - 1] = arr[i] - 1 
-          #arr[i] = arr[i] + 1
-          arr[i + 1] = arr[i] + 1
-          count += 1 
-          print(arr[i])
-          print("- 1")
-        elif arr[i + 1] >= arr[i] and arr[i + 1] <= arr[i - 1]:
-          print(arr[i])
-          arr[i] = arr[i - 1] + 1
-          count += 1
-          print(arr[i])
-          print("- 2")
-      elif arr[i + 1] <= arr[i] and arr[i] <= arr[i - 1]:
-        print(arr[i])
-        arr[i] = arr[i - 1] + 1
-        arr[i + 1] = arr[i] + 1
-        count += 2
-        testcount +=1
-        print(arr[i])
-        print("- 5")
-      elif arr[i + 1] >= arr[i] and arr[i + 1] <= arr[i - 1]:
-        print(arr[i])
-        arr[i] = arr[i - 1] + 1
-        count += 1 
-        print(arr[i]) 
-        print("- 3")
-      elif arr[i + 1] >= arr[i] and arr[i - 1] >= arr[i]:
-        if arr[i + 1] > arr[i - 1] :
-          print(arr[i])
-          arr[i] = arr[i - 1] + 1
-          count += 1 
-          print(arr[i]) 
-          print("- 6") 
-        elif arr[i + 1] < arr[i - 1] :
-          print(arr[i])
-          arr[i] = arr[i - 1] + 1
-          arr[i + 1] = arr[i] + 1
-          count += 1 
-          print(arr[i]) 
-          print("- 7")           
+    if arr[i + 1] <= arr[i] and arr[i] < 1000000000:
+      arr[i + 1] = arr[i] + 1
+      count += 1
     elif arr[i] >= 1000000000:
-      print(arr[i])
       arr[i] = arr[i - 1] + 1
-      #count += 1 
-      print(arr[i])
-      print("- 4")
+      #count += 1
+      if arr[i + 1] <= arr[i]:
+        arr[i + 1] = arr[i] + 1
+        #count += 1
 
-  print(arr)
-  print(testcount)
+  print(arr)  
   return count
 
 
