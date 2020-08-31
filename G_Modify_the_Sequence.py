dd19a55b01d012846c96df8bd4f8d5edba282150
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-08-31 12:31:29 +0000

    Still trying

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -12,23 +12,63 @@ def modifySequence(arr):
   #arr = sorted(arr , reverse = True)
   #print(arr)
   for i in range(len(arr)-1):
-    if arr[i] >= 1000000000:
-      arr[i] = arr[i - 1]
-      
-    
-  for i in range(len(arr)-1):
-    if arr[i +  1] <= arr[i] :
-      arr[i+1] = arr[i] + 1
-      count += 1
-    
-    
-    
-    
-   
-      
-        
-      
+    print(arr[i])
+    print("- 0")
+    if arr[i] < 1000000000:
+      if arr[i + 1] <= arr[i] and arr[i] > arr[i - 1]:
+        if arr[i] > arr[i - 1]:
+          print(arr[i - 1])
+          print(arr[i])
+          print(arr[i + 1])
+          #arr[i - 1] = arr[i] - 1 
+          #arr[i] = arr[i] + 1
+          arr[i + 1] = arr[i] + 1
+          count += 1 
+          print(arr[i])
+          print("- 1")
+        elif arr[i + 1] >= arr[i] and arr[i + 1] <= arr[i - 1]:
+          print(arr[i])
+          arr[i] = arr[i - 1] + 1
+          count += 1
+          print(arr[i])
+          print("- 2")
+      elif arr[i + 1] <= arr[i] and arr[i] <= arr[i - 1]:
+        print(arr[i])
+        arr[i] = arr[i - 1] + 1
+        arr[i + 1] = arr[i] + 1
+        count += 2
+        testcount +=1
+        print(arr[i])
+        print("- 5")
+      elif arr[i + 1] >= arr[i] and arr[i + 1] <= arr[i - 1]:
+        print(arr[i])
+        arr[i] = arr[i - 1] + 1
+        count += 1 
+        print(arr[i]) 
+        print("- 3")
+      elif arr[i + 1] >= arr[i] and arr[i - 1] >= arr[i]:
+        if arr[i + 1] > arr[i - 1] :
+          print(arr[i])
+          arr[i] = arr[i - 1] + 1
+          count += 1 
+          print(arr[i]) 
+          print("- 6") 
+        elif arr[i + 1] < arr[i - 1] :
+          print(arr[i])
+          arr[i] = arr[i - 1] + 1
+          arr[i + 1] = arr[i] + 1
+          count += 1 
+          print(arr[i]) 
+          print("- 7")           
+    elif arr[i] >= 1000000000:
+      print(arr[i])
+      arr[i] = arr[i - 1] + 1
+      #count += 1 
+      print(arr[i])
+      print("- 4")
+
   print(arr)
+  print(testcount)
   return count
 
 
