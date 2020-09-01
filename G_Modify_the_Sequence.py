94a1c639b761d784454d3bf4b010340f0e8123fb
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-09-01 12:28:53 +0000

    Trying

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -10,17 +10,50 @@ def modifySequence(arr):
   totallen = len(arr)
   startSequence = 1000000000 - totallen
   #arr = sorted(arr , reverse = True)
-  #print(arr)
-  for i in range(len(arr)-1):
-    if arr[i + 1] <= arr[i] and arr[i] < 1000000000:
-      arr[i + 1] = arr[i] + 1
-      count += 1
-    elif arr[i] >= 1000000000:
-      arr[i] = arr[i - 1] + 1
-      #count += 1
-      if arr[i + 1] <= arr[i]:
-        arr[i + 1] = arr[i] + 1
+  for i in range(len(arr) -1):
+    if arr[i] < arr[i + 1]:
+      doNothing = 1
+      print(arr[i])
+      print("- 1")
+    else:
+      if arr[i] == arr[i + 1]:
+        print(arr[i])
+        print(arr[i+1])
+        if arr[i + 1] < 1000000000:
+          arr[i + 1] = arr[i + 1] + 1
+          count += 1
+        else:
+          arr[i] =  arr[i - 1] + 1
+          arr[i + 1] = arr[i] + 1
+          count += 2
+        #arr[i + 1] = arr[i + 1] + 1
+        
+        print(arr[i])
+        print("- 2")
+      elif arr[i] > arr[i + 1]:
+        print(arr[i])
+        if arr[i + 1] < 1000000000:
+          checkingVar = arr[i + 1] - 1
+        else:
+          checkingVar = arr[i - 1] + 1
         #count += 1
+        if checkingVar == arr[i-1]:
+          arr[i-1] = checkingVar - 1
+          arr[i] = checkingVar
+          arr[i+1] = checkingVar + 1
+          count += 3
+          print(arr[i])
+          print("- 4")
+        elif checkingVar < arr[i-1]:
+          arr[i] = arr[i-1] + 1
+          arr[i+1] = arr[i] + 1
+          count += 2
+          print(arr[i])
+          print("- 5")
+        else:
+          arr[i] = checkingVar
+          print(arr[i])
+          print("- 3")       
 
   print(arr)  
   return count
