e486690e3efb077f222dee4d6110ee88fad77135
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-09-02 18:19:45 +0000

    still trying

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -5,55 +5,41 @@ import sys
 # Complete the modifySequence function below.
 #
 def modifySequence(arr):
+
   count = 0  
   testcount = 0
   totallen = len(arr)
   startSequence = 1000000000 - totallen
+  
   #arr = sorted(arr , reverse = True)
-  for i in range(len(arr) -1):
-    if arr[i] < arr[i + 1]:
-      doNothing = 1
-      print(arr[i])
-      print("- 1")
-    else:
-      if arr[i] == arr[i + 1]:
-        print(arr[i])
-        print(arr[i+1])
-        if arr[i + 1] < 1000000000:
-          arr[i + 1] = arr[i + 1] + 1
+  if len(arr) > 1:
+    for i in range(len(arr) -1):
+      
+      first = arr[i]
+      second = arr[i + 1] if i < len(arr) -1 else 0
+      third = arr[i + 2] if i < len(arr) -2 else 0
+
+      if first >= second and second >= third and i < len(arr) -2:
+        if first >= 1000000000 and second < 1000000000 and third < 1000000000:
+          arr[i + 1] = first + 1
+          arr[i + 2] =  arr[i + 1] + 1
+        count += 2
+      elif first >= second and second <= third  and i < len(arr) -2:
+        if first < third:
+          arr[i + 1] = arr[i + 2] - 1
           count += 1
         else:
-          arr[i] =  arr[i - 1] + 1
-          arr[i + 1] = arr[i] + 1
+          arr[i + 1] = first + 1
+          arr[i + 2] =  arr[i + 1] + 1
           count += 2
-        #arr[i + 1] = arr[i + 1] + 1
-        
-        print(arr[i])
-        print("- 2")
-      elif arr[i] > arr[i + 1]:
-        print(arr[i])
-        if arr[i + 1] < 1000000000:
-          checkingVar = arr[i + 1] - 1
+      elif first <= second and second >= third  and i < len(arr) -2:
+        if first < third:
+          arr[i + 1] = arr[i + 2] - 1
+          count += 1
         else:
-          checkingVar = arr[i - 1] + 1
-        #count += 1
-        if checkingVar == arr[i-1]:
-          arr[i-1] = checkingVar - 1
-          arr[i] = checkingVar
-          arr[i+1] = checkingVar + 1
-          count += 3
-          print(arr[i])
-          print("- 4")
-        elif checkingVar < arr[i-1]:
-          arr[i] = arr[i-1] + 1
-          arr[i+1] = arr[i] + 1
+          arr[i + 1] = first + 1
+          arr[i + 2] =  arr[i + 1] + 1
           count += 2
-          print(arr[i])
-          print("- 5")
-        else:
-          arr[i] = checkingVar
-          print(arr[i])
-          print("- 3")       
 
   print(arr)  
   return count
