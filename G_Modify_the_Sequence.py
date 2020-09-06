5a489b6ed1a58ffe70393ee665b3317003320eb6
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-09-06 12:26:30 +0000

    Still working

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -10,19 +10,50 @@ def modifySequence(arr):
   testcount = 0
   totallen = len(arr)
   count = totallen
+  countSequence = 0
+  firstOftheSeq = []
   startSequence = 1000000000 - totallen
   
   #arr = sorted(arr , reverse = True)
   if len(arr) > 1:
     for i in range(0 , len(arr)-1 , 1):
-      #print(i)
-      if arr[i] <= arr[i + 1] or arr[i] == arr[i - 1] and arr[i] < 1000000000:
-        count = count - 1
-      elif arr[i] >= 1000000000:
-        count = count - 1
+      print(arr[i])
+      print("- 1")
+      countSequence = 0 
+      firstOftheSeq = []       
+      for j in range(i , len(arr)-1 , 1):
+        print(arr[j])
+        print("- 2")
+        if arr[j] < arr[j + 1]:
+          if arr[j] < 1000000000 and arr[j + 1] < 1000000000:
+            firstOftheSeq.append(arr[j])
+            firstOftheSeq.append(arr[j + 1])
+            countSequence += 1            
+            print("- 3")
+            print(countSequence)
+          elif arr[j] >= 1000000000:
+            
+            firstOftheSeq.append(arr[j])
+            firstOftheSeq.append(arr[j + 1])
+            countSequence += 1            
+            print("- 4")
+            print(countSequence)
+          elif arr[j] >= 1000000000:
+            firstOftheSeq.append(arr[j])
+            firstOftheSeq.append(arr[j + 1])
+            countSequence += 1            
+            print("- 5")
+            print(countSequence)
+        else:
+          print(firstOftheSeq)  
+          if countSequence > 0:
+            if arr[j]
+            arr[i] = arr[i + 1] + 1
+            count += 1
 
-  #print(arr)  
-  return totallen - count
+
+ 
+  return count
 
 
 
