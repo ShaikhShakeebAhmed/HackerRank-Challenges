8a8fbcdf603a7e70e9c10e9d6e115a5733a84718
Shakeeb Ahmed <Shakeebazaheer@gmail.com>
2020-09-06 07:27:04 +0000

    starting at it again

--- G_Modify_the_Sequence.py
+++ G_Modify_the_Sequence.py
@@ -9,18 +9,20 @@ def modifySequence(arr):
   count = 0  
   testcount = 0
   totallen = len(arr)
+  count = totallen
   startSequence = 1000000000 - totallen
   
   #arr = sorted(arr , reverse = True)
   if len(arr) > 1:
-    for i in range(len(arr)):
-      if arr[i] < arr[i + 1]:
-        totallen = totallen - 1
-
-     
+    for i in range(0 , len(arr)-1 , 1):
+      #print(i)
+      if arr[i] <= arr[i + 1] or arr[i] == arr[i - 1] and arr[i] < 1000000000:
+        count = count - 1
+      elif arr[i] >= 1000000000:
+        count = count - 1
 
   #print(arr)  
-  return totallen
+  return totallen - count
 
 
 
